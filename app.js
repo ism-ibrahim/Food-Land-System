af5b72abe25b0af2ca271af8327919426964df35
ism-ibrahim <iboss-97+second@mail.ru>
2023-02-19 14:34:59 +0000

    edit

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 "use strict";
 $(document).ready(function () {
 
-    $('.btn').click(function (e) {
-        e.preventDefault();
-        let f = a => {
-            if (Number(a)) {
-                for (let i = 1; i <= a; i++) {
-                    $('.textbox').append(`<h2>${a}</h2>`);
-                }
-            } else{
-                $('.textbox').append(`<h2>${a}</h2>`);
-            }
-        }
-        f ($('input').val());
+    // $('.btn').click(function (e) {
+    //     e.preventDefault();
+    //     let f = a => {
+    //         if (Number(a)) {
+    //             for (let i = 1; i <= a; i++) {
+    //                 $('.textbox').append(`<h2>${a}</h2>`);
+    //             }
+    //         } else{
+    //             $('.textbox').append(`<h2>${a}</h2>`);
+    //         }
+    //     }
+    //     f ($('input').val());
 
-    });
+    // });
 
 });
